adc5b28d1bfa73b3faee105f7b43eff508ac3c9a
AGT-Slade <83044137+AGT-Slade@users.noreply.github.com>
2022-10-01 20:18:17 -0600

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -13,3 +13,23 @@ toggle.onclick = function(){
   toggle.classList.toggle('active');
   sidebar.classList.toggle('active');
     }
+
+    // Get the button
+    let mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
